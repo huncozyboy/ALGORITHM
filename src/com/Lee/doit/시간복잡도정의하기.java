89566b76e6f7483334800c4134bc31278f34a1eb
huncozyboy <leejh5838@naver.com>
2025-01-12 06:13:18 +0900

    feat : day10-시간복잡도정의하기

---
+++ src/com/Lee/doit/시간복잡도정의하기.java
@@ -0,0 +1,13 @@
+package com.Lee.doit;
+
+public class 시간복잡도정의하기 {
+    public static void main(String[] args) {
+        int findNumber = (int)(Math.random() * 100);
+        for(int i = 0; i < 100; i++) {
+            if(i == findNumber) {
+                System.out.println("findNumber: " + findNumber);
+                break;
+            }
+        }
+    }
+}
