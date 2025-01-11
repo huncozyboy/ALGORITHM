746505ca4458c0e52998e74d361fcbd487a0591d
huncozyboy <leejh5838@naver.com>
2025-01-12 06:22:50 +0900

    feat : day10-수정렬하기

---
+++ src/com/Lee/doit/수정렬하기.java
@@ -0,0 +1,25 @@
+package com.Lee.doit;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class 수정렬하기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] num = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            num[i] = sc.nextInt();
+        }
+
+        Arrays.sort(num);
+
+        for (int n : num) {
+            System.out.println(n);
+        }
+
+        sc.close();
+    }
+}
