1875efa38667cb2ec6a8400673ae4b8ea16afa30
huncozyboy <leejh5838@naver.com>
2025-01-22 23:49:02 +0900

    feat : day13-소수만들기

---
+++ src/com/Lee/프로그래머스/소수만들기.java
@@ -0,0 +1,47 @@
+package com.Lee.프로그래머스;
+
+import java.util.*;
+
+public class 소수만들기 {
+
+    static int answer;
+
+    static public int solution(int[] nums) {
+        answer = 0;
+
+        HashSet<Integer> set = new HashSet<>(); // hashset을 이용해서 중복 제거
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    System.out.println(nums[i] + " " + nums[j] + " " + nums[k]);
+                    int num = nums[i] + nums[j] + nums[k];
+                    if (isPrime(num)) {
+                        answer++;
+                    }
+                }
+            }
+
+        }
+
+        return answer;
+    }
+
+    static boolean isPrime(int n) {
+        if (n == 0 || n == 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(solution(new int[]{1, 2, 7, 6, 4})); // 4
+    }
+}
