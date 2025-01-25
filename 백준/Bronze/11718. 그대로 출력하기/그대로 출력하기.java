2e9e936d5b46c2bccf7a2cb045bcff1b0261b38e
JiHun Lee <163561527+huncozyboy@users.noreply.github.com>
2025-01-26 08:21:50 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 104 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String line;
+
+        while ((line = br.readLine()) != null) {
+            bw.write(line);
+            bw.newLine(); 
+        }
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
