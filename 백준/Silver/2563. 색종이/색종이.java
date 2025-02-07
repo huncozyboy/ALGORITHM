fc7b765a75f31d3f19c42169398315c362a3f8f8
JiHun Lee <163561527+huncozyboy@users.noreply.github.com>
2025-02-08 00:43:17 +0900

    [Silver V] Title: 색종이, Time: 104 ms, Memory: 14096 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int count = Integer.parseInt(bufferedReader.readLine());
+        int x = 0;
+        int y = 0;
+        int totalArea = 0;
+        boolean[][] paper = new boolean[100][100];
+
+        for (int i = 0; i < count; i++) {
+            StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            x = Integer.parseInt(stringTokenizer.nextToken());
+            y = Integer.parseInt(stringTokenizer.nextToken());
+            for (int j = x; j < x + 10; j++) {
+                for (int k = y; k < y + 10; k++) {
+                    if (!paper[j][k]) {
+                        paper[j][k] = true;
+                        totalArea++;
+                    }
+                }
+
+            }
+        }
+        System.out.println(totalArea);
+    }
+}
