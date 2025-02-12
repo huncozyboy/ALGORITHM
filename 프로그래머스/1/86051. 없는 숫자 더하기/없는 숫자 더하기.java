c4279fca8de3492fee1414df26c9a1c7d7f32bb6
JiHun Lee <163561527+huncozyboy@users.noreply.github.com>
2025-02-13 01:04:17 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.02 ms, Memory: 91 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,24 @@
+class Solution {
+    static public int solution(int[] numbers) {
+        int answer = 0;
+
+        int[] arr = {1,1,1,1,1,1,1,1,1,1};
+
+        for(int i=0; i<numbers.length; i++) {
+            arr[numbers[i]] --;
+        }
+
+        for(int i=0; i<arr.length; i++) {
+            if(arr[i] == 1) {
+                answer += i;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {1,2,3,4,6,7,8,0};
+        System.out.println(solution(numbers));
+    }
+}
